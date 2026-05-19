d23a22549fe8b5f53fe38f587c7e9c5fabfe9655
abd <abd.shaar.506@gmail.com>
2026-05-20 01:46:47 +0200

    Section Random user

--- app/controllers/pagesController.php
+++ app/controllers/pagesController.php
@@ -3,17 +3,21 @@
 
 namespace App\Controllers\PagesController;
 
-
 use \PDO;
 use \App\Models\RecipesModel;
+use \App\Models\UsersModel;
+
+
 
 
 function homeAction(PDO $conn)
 {
     include_once '../app/models/recipesModel.php';
+    include_once '../app/models/usersModel.php';
     $randomRecipe = RecipesModel\findOneByRand($conn);
-
     $popularRecipe = RecipesModel\findManyByRand($conn);
+    $randomUser = UsersModel\findOneByRand($conn);
+    $userLatestRecipes = RecipesModel\findAllByUserId($conn, $randomUser['id']);
 
 
     global $title, $content;
@@ -22,3 +26,6 @@ function homeAction(PDO $conn)
     include '../app/views/pages/home.php';
     $content = ob_get_clean();
 }
+
+
+
--- app/models/recipesModel.php
+++ app/models/recipesModel.php
@@ -27,3 +27,17 @@ function findManyByRand(PDO $conn): array
     $rs = $conn->query($sql);
     return $rs->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function findAllByUserId(PDO $conn, int $userID)
+{
+    $sql = "SELECT *
+            FROM recipes
+            WHERE user_id = :userID
+            ORDER BY created_at DESC
+            LIMIT 3;";
+    $rs = $conn->prepare($sql);
+    $rs->bindValue(':userID', $userID, PDO::PARAM_INT);
+    $rs->execute();
+    return $rs->fetchAll(PDO::FETCH_ASSOC);
+}
+
