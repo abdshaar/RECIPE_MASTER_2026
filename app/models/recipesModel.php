5845660410b033f3b0850f05fa120d6b7e535476
abd <abd.shaar.506@gmail.com>
2026-05-26 01:37:38 +0200

    Route recipes.show

--- app/models/recipesModel.php
+++ app/models/recipesModel.php
@@ -41,3 +41,16 @@ function findAllByUserId(PDO $conn, int $userID)
     return $rs->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function findOneById(PDO $conn, int $id)
+{
+    $sql = "SELECT *
+            FROM recipes
+            WHERE id = :id";
+    $rs = $conn->prepare($sql);
+    $rs->bindValue(':id', $id, PDO::PARAM_INT);
+    $rs->execute();
+    return $rs->fetch(PDO::FETCH_ASSOC);
+}
+
+
+
